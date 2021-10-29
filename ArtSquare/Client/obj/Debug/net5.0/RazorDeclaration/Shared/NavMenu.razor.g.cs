634a4ec2738a1dd5b15dbaa425ff09e0f051e7c5
Senad <senad.tosuni98@student.hogent.be>
2021-10-29 11:33:46 +0200

    Auction Added

--- ArtSquare/Client/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ ArtSquare/Client/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -13,77 +13,77 @@ namespace ArtSquare.Client.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\szabo\Desktop\5. Semester HoGent\DevOps\finalVersion\ArtSquare\Client\_Imports.razor"
+#line 1 "C:\Users\senad\DevOps\devops-project-web-ic\ArtSquare\Client\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\szabo\Desktop\5. Semester HoGent\DevOps\finalVersion\ArtSquare\Client\_Imports.razor"
+#line 2 "C:\Users\senad\DevOps\devops-project-web-ic\ArtSquare\Client\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\szabo\Desktop\5. Semester HoGent\DevOps\finalVersion\ArtSquare\Client\_Imports.razor"
+#line 3 "C:\Users\senad\DevOps\devops-project-web-ic\ArtSquare\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\szabo\Desktop\5. Semester HoGent\DevOps\finalVersion\ArtSquare\Client\_Imports.razor"
+#line 4 "C:\Users\senad\DevOps\devops-project-web-ic\ArtSquare\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\szabo\Desktop\5. Semester HoGent\DevOps\finalVersion\ArtSquare\Client\_Imports.razor"
+#line 5 "C:\Users\senad\DevOps\devops-project-web-ic\ArtSquare\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\szabo\Desktop\5. Semester HoGent\DevOps\finalVersion\ArtSquare\Client\_Imports.razor"
+#line 6 "C:\Users\senad\DevOps\devops-project-web-ic\ArtSquare\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\szabo\Desktop\5. Semester HoGent\DevOps\finalVersion\ArtSquare\Client\_Imports.razor"
+#line 7 "C:\Users\senad\DevOps\devops-project-web-ic\ArtSquare\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\szabo\Desktop\5. Semester HoGent\DevOps\finalVersion\ArtSquare\Client\_Imports.razor"
+#line 8 "C:\Users\senad\DevOps\devops-project-web-ic\ArtSquare\Client\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\szabo\Desktop\5. Semester HoGent\DevOps\finalVersion\ArtSquare\Client\_Imports.razor"
+#line 9 "C:\Users\senad\DevOps\devops-project-web-ic\ArtSquare\Client\_Imports.razor"
 using ArtSquare.Client;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\szabo\Desktop\5. Semester HoGent\DevOps\finalVersion\ArtSquare\Client\_Imports.razor"
+#line 10 "C:\Users\senad\DevOps\devops-project-web-ic\ArtSquare\Client\_Imports.razor"
 using ArtSquare.Client.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\szabo\Desktop\5. Semester HoGent\DevOps\finalVersion\ArtSquare\Client\_Imports.razor"
+#line 11 "C:\Users\senad\DevOps\devops-project-web-ic\ArtSquare\Client\_Imports.razor"
 using MudBlazor;
 
 #line default
@@ -97,7 +97,7 @@ using MudBlazor;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 56 "C:\Users\szabo\Desktop\5. Semester HoGent\DevOps\finalVersion\ArtSquare\Client\Shared\NavMenu.razor"
+#line 56 "C:\Users\senad\DevOps\devops-project-web-ic\ArtSquare\Client\Shared\NavMenu.razor"
       
     MudTheme MyCustomTheme = new MudTheme()
     {
@@ -119,7 +119,7 @@ using MudBlazor;
 #line hidden
 #nullable disable
 #nullable restore
-#line 73 "C:\Users\szabo\Desktop\5. Semester HoGent\DevOps\finalVersion\ArtSquare\Client\Shared\NavMenu.razor"
+#line 73 "C:\Users\senad\DevOps\devops-project-web-ic\ArtSquare\Client\Shared\NavMenu.razor"
        
     bool _drawerOpen = true;
 
