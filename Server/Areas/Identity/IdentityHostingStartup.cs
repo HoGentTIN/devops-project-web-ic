3b05e578b10c8ac5b4f624e0cacd5788954c39ef
AsierPerez414 <asierperez45@gmail.com>
2021-11-11 16:31:32 +0100

    Login added but with local database

---
+++ Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ArtSquare.Server.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ArtSquare.Server.Areas.Identity.IdentityHostingStartup))]
+namespace ArtSquare.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ArtSquareServerContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("ArtSquareServerContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ArtSquareServerContext>();
+            });
+        }
+    }
+}
